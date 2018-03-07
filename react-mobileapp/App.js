5a383839205a191b7a53b3ac08a0491ed6fbb5b7
FLorian demol <florian.demol13@gmail.com>
2018-03-07 13:39:17 +0100

    react native

--- react-mobileapp/App.js
+++ react-mobileapp/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from './src/components/login/login';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View /*style={styles.container}*/>
-
-      <Login/>
-
-      </View>
+        <Login />
     );
   }
 }
