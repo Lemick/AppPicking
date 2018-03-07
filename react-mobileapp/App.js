383bb0ca994a33026c02b1959072a98996aefcc3
FLorian demol <florian.demol13@gmail.com>
2018-03-07 18:21:21 +0100

    login mobile

--- react-mobileapp/App.js
+++ react-mobileapp/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/components/login.js';
 
 export default class App extends React.Component {
   render() {
     return (
-        <Login />
+     <Login />
     );
   }
 }
--- react-mobileapp/src/components/login.js
+++ react-mobileapp/src/components/login.js
@@ -1,45 +1,73 @@
+//https://github.com/gcanti/tcomb-form-native
+
 import { ScrollView, TextInput } from "react-native-gesture-handler";
 import Container from "./Container";
 import Label from "./Label";
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import t from 'tcomb-form-native';
+
+var Form = t.form.Form;
+
+var Person = t.struct({
+    Utilisateur: t.String,              // a required string
+    'Mot de passe': t.String  // password
+  });
+
+var options = {
+    fields:{
+        'Mot de passe':{
+            password: true,
+            secureTextEntry: true
+        }
+    }
+};
 
 export default class Login extends Component {
     render() {
         return (
-            <ScrollView style={styles.scroll}>
-                <Container>
-                    <Label text="Utilisateur" />
-                    <TextInput
-                        style={styles.textInput}
-                    />    
-                </Container>
-                <Container>
-                    <Label text="Mot de passe" />
-                    <TextInput
-                        secureTextEntry={true}
-                        style={styles.textInput}
-                    />
-                </Container>    
-            </ScrollView>
+            <View style={styles.container}>
+                {/* display */}
+                <Text style={styles.title}> HuitNeufDis </Text>
+                <Form
+                    ref="form"
+                    type={Person}
+                    options = {options}
+                />
+                <TouchableHighlight style={styles.button} onPress={this.onPress} underlayColor='#99d9f4'>
+                    <Text style={styles.buttonText}>Save</Text>
+                </TouchableHighlight>
+            </View>      
         );
     }
 }
 
+
 const styles = StyleSheet.create({
-    scroll: {
-        backgroundColor : '#E1D7D8',
-        padding: 30,
-        flexDirection: 'column'
-    },
-    label: {
-        color: '#0d8898',
-        fontSize: 20
-    },
-    alignRight: {
-        alignSelf: 'flex-end'
-    },
-    textInput: {
-        heigth: 80,
+    container: {
+        justifyContent: 'center',
+        marginTop: 100,
+        padding: 20,
+        backgroundColor: '#ffffff',
+      },
+      title: {
         fontSize: 30,
-        backgroundColor: '#FFF'
-    }    
+        alignSelf: 'center',
+        marginBottom: 60
+      },
+      buttonText: {
+        fontSize: 18,
+        color: 'white',
+        alignSelf: 'center'
+      },
+      button: {
+        height: 36,
+        backgroundColor: '#48BBEC',
+        borderColor: '#48BBEC',
+        borderWidth: 1,
+        borderRadius: 8,
+        marginBottom: 10,
+        alignSelf: 'stretch',
+        justifyContent: 'center'
+      },
 });
